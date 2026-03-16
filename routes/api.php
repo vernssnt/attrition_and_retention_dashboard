0d040a12cff6b7ee82ce781849a18208bf3f7603
vernssnt <verns2728@gmail.com>
2026-03-17 06:28:08 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Api\StudentApiController;
 
 /*
@@ -16,3 +17,7 @@ Route::get('/students', [StudentApiController::class, 'index']);
 Route::get('/dashboard-summary', [StudentApiController::class, 'dashboardSummary']);
 
 Route::get('/risk-distribution', [StudentApiController::class, 'riskDistribution']);
+
+Route::get('/powerbi/students', function () {
+    return DB::table('students')->get();
+});
