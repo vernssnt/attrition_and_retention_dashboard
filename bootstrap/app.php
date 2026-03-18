26aa13ceab59b311257ccf2796ad63958fd9a2e8
vernssnt <verns2728@gmail.com>
2026-03-19 03:32:10 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->trustProxies(at: '*'); // ✅ ADD THIS LINE
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
